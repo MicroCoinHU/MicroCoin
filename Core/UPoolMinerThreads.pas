1d58e2282be699ec5bff1a9c243d7d45df69ac71
Pethical <sokkis@gmail.com>
2018-08-16 15:37:25 +0200

    fix year

--- Core/UPoolMinerThreads.pas
+++ Core/UPoolMinerThreads.pas
@@ -3,7 +3,7 @@ unit UPoolMinerThreads;
 {$mode delphi}
 {$endif}
 { 
-  Copyright (c) Albert Molina 2016 - <YEAR YOU FORKED> original code from PascalCoin https://pascalcoin.org/
+  Copyright (c) Albert Molina 2016 - 2018 original code from PascalCoin https://pascalcoin.org/
 
   Distributed under the MIT software license, see the accompanying file LICENSE
   or visit http://www.opensource.org/licenses/mit-license.php.
