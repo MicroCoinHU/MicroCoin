baa3e5ea8d349ce2de6ba11e3b7e2b8ac99b9676
unknown <sokkis@gmail.com>
2018-08-28 13:19:49 +0200

    subaccounts + refractorings

---
+++ Core/BlockChain/MicroCoin.BlockChain.Events.pas
@@ -0,0 +1,86 @@
+unit MicroCoin.BlockChain.Events;
+{
+  This unit contains code from PascalCoin:
+
+  Copyright (c) Albert Molina 2016 - 2018 original code from PascalCoin https://pascalcoin.org/
+
+  Distributed under the MIT software license, see the accompanying file LICENSE
+  or visit http://www.opensource.org/licenses/mit-license.php.
+
+}
+
+interface
+
+uses UThread, MicroCoin.Transaction.HashTree,
+     MicroCoin.BlockChain.Block, ULog, SysUtils,
+     MicroCoin.Transaction.Events, MicroCoin.Net.Connection;
+
+type
+
+  TNotifyNewBlockThread = class(TPCThread)
+  FNetConnection: TNetConnection;
+  FSanitizedOperationsHashTree: TTransactionHashTree;
+  FNewBlockOperations: TBlock;
+protected
+  procedure BCExecute; override;
+public
+  constructor Create(NetConnection: TNetConnection; MakeACopyOfNewBlockOperations: TBlock; MakeACopyOfSanitizedOperationsHashTree: TTransactionHashTree);
+  destructor Destroy; override;
+  end;
+
+
+implementation
+
+uses
+  MicroCoin.Net.ConnectionManager;
+
+procedure TNotifyNewBlockThread.BCExecute;
+begin
+  DebugStep := 'Locking';
+  if TConnectionManager.NetData.ConnectionLock(Self, FNetConnection, 500) then
+  begin
+    try
+      DebugStep := 'Checking connected';
+      if not FNetConnection.Connected then
+        exit;
+      TLog.NewLog(ltdebug, Classname, 'Sending new block found to ' + FNetConnection.Client.ClientRemoteAddr);
+      DebugStep := 'Sending';
+      FNetConnection.Send_NewBlockFound(FNewBlockOperations);
+      DebugStep := 'Checking connected again';
+      if not FNetConnection.Connected then
+        exit;
+      DebugStep := 'Need send opreations?';
+      if FSanitizedOperationsHashTree.OperationsCount > 0 then
+      begin
+        DebugStep := 'Sending ' + IntToStr(FSanitizedOperationsHashTree.OperationsCount) + ' sanitized operations';
+        TLog.NewLog(ltdebug, Classname, 'Sending ' + IntToStr(FSanitizedOperationsHashTree.OperationsCount) + ' sanitized operations to ' + FNetConnection.ClientRemoteAddr);
+        TNotifyTransactionThread.Create(FNetConnection, FSanitizedOperationsHashTree);
+      end;
+      DebugStep := 'Unlocking';
+    finally
+      TConnectionManager.NetData.ConnectionUnlock(FNetConnection);
+    end;
+  end;
+  DebugStep := 'Finalizing';
+end;
+
+constructor TNotifyNewBlockThread.Create(NetConnection: TNetConnection; MakeACopyOfNewBlockOperations: TBlock; MakeACopyOfSanitizedOperationsHashTree: TTransactionHashTree);
+begin
+  FNetConnection := NetConnection;
+  FSanitizedOperationsHashTree := TTransactionHashTree.Create;
+  FSanitizedOperationsHashTree.CopyFromHashTree(MakeACopyOfSanitizedOperationsHashTree);
+  FNewBlockOperations := TBlock.Create(nil);
+  FNewBlockOperations.CopyFrom(MakeACopyOfNewBlockOperations);
+  inherited Create(false);
+  FreeOnTerminate := true;
+end;
+
+destructor TNotifyNewBlockThread.Destroy;
+begin
+  FreeAndNil(FSanitizedOperationsHashTree);
+  FreeAndNil(FNewBlockOperations);
+  inherited;
+end;
+
+
+end.
--- Core/Node/MicroCoin.Node.Events.pas
+++ Core/Node/MicroCoin.Node.Events.pas
@@ -1,5 +1,13 @@
 unit MicroCoin.Node.Events;
+{
+  This unit contains code from PascalCoin:
 
+  Copyright (c) Albert Molina 2016 - 2018 original code from PascalCoin https://pascalcoin.org/
+
+  Distributed under the MIT software license, see the accompanying file LICENSE
+  or visit http://www.opensource.org/licenses/mit-license.php.
+}
+
 interface
 
 uses MicroCoin.Node.Node, Sysutils, classes, MicroCoin.Net.Connection, UThread, UCrypto, ULog;
